1ab38e61c9d5fe0bbb8980abc85a496bc08e28c6
joewxlker <joewxlk3r@gmail.com>
2026-03-26 17:15:28 +0400

    fix PROJECT_ROOT env var setting issue

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
 
 fn main() {
-    let project_root = PathBuf::from(std::env::var("PROJECT_ROOT").expect("PROJECT_ROOT not set"));
+    let project_root_str = std::env::var("PROJECT_ROOT").expect("PROJECT_ROOT not set");
+    let project_root = PathBuf::from(&project_root_str);
 
     assert!(project_root.exists(), "project_root: {:?} does not exist", project_root);
 
@@ -9,7 +10,7 @@ fn main() {
     std::fs::create_dir_all(project_root.join("cache/images")).unwrap();
     std::fs::create_dir_all(project_root.join("logs")).unwrap();
 
-    println!("cargo:rustc-env=PROJECT_ROOT={:?}", project_root);
+    println!("cargo:rustc-env=PROJECT_ROOT={:?}", project_root_str);
 
     println!("cargo:rerun-if-changed=build.rs");
 }
