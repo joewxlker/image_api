399fb5c176800fed175b2e1e120dbcb56cb25973
joewxlker <joewxlk3r@gmail.com>
2026-04-19 20:44:22 +0400

    trace otelcol

--- src/main.rs
+++ src/main.rs
@@ -133,7 +133,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build();
 
     let otel_layer =
-        OpenTelemetryTracingBridge::new(&logger_provider).with_filter(EnvFilter::new("warn"));
+        OpenTelemetryTracingBridge::new(&logger_provider).with_filter(EnvFilter::new("trace"));
 
     let fmt_layer = tracing_subscriber::fmt::layer()
         .with_writer(log_file)
