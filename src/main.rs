517114311781d9f460b8dc72def4f34a21325422
joewxlker <joewxlk3r@gmail.com>
2026-04-21 11:19:55 +0400

    make metrics/logging optional

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@ use std::path::PathBuf;
 
 use figment::Figment;
 use figment::providers::{Format, Toml};
-use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge as OtelBridge;
 use opentelemetry_sdk::logs::{BatchConfig, BatchLogProcessor, SdkLoggerProvider};
 use serde::{Deserialize, Serialize};
 use tokio::select;
 use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Layer};
 use url::Url;
 
@@ -63,7 +62,9 @@ struct Package {
 #[derive(Deserialize, Serialize)]
 struct OTLP {
     instance_id: String,
-    collector_endpoint: Url,
+    collector_endpoint: Option<Url>,
+    collect_logs: Option<bool>,
+    collect_metrics: Option<bool>,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -75,15 +76,122 @@ struct Config {
     image_cache_directory: PathBuf,
 }
 
-fn get_resource(config: &Config) -> Resource {
-    Resource::builder()
-        .with_attributes([
-            KeyValue::new("service.name", config.package.name.clone()),
-            KeyValue::new("service.version", config.package.version.clone()),
-            KeyValue::new("service.instance.id", config.otlp.instance_id.clone()),
-            KeyValue::new("environment", config.environment.clone()),
-        ])
-        .build()
+pub struct Otel {
+    meter_provider: Option<SdkMeterProvider>,
+    logs_provider: Option<SdkLoggerProvider>,
+}
+
+impl Otel {
+    pub fn default() -> Self {
+        Self {
+            logs_provider: None,
+            meter_provider: None,
+        }
+    }
+    fn get_resource(config: &Config) -> Resource {
+        Resource::builder()
+            .with_attributes([
+                KeyValue::new("service.name", config.package.name.clone()),
+                KeyValue::new("service.version", config.package.version.clone()),
+                KeyValue::new("service.instance.id", config.otlp.instance_id.clone()),
+                KeyValue::new("environment", config.environment.clone()),
+            ])
+            .build()
+    }
+    async fn from_config(config: &Config) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut this = Self::default();
+        let resource = Self::get_resource(&config);
+
+        if let Some(ref collector_endpoint) = config.otlp.collector_endpoint {
+            if !(config.otlp.collect_logs == Some(false)) {
+                println!(
+                    "Initializing OTLP logs exporter with collector endpoint: {}",
+                    collector_endpoint
+                );
+                this.logs_provider = Some(Self::init_logger(collector_endpoint, &resource).await?);
+            } else {
+                println!("OTLP logs collection is disabled by configuration.");
+            }
+
+            if !(config.otlp.collect_metrics == Some(false)) {
+                println!(
+                    "Initializing OTLP metrics exporter with collector endpoint: {}",
+                    collector_endpoint
+                );
+                this.meter_provider = Some(Self::init_meter(collector_endpoint, &resource).await?);
+            } else {
+                println!("OTLP metrics collection is disabled by configuration.");
+            }
+        } else {
+            println!(
+                "OTLP collector endpoint is not configured; logs and metrics exporters will not be initialized."
+            );
+        }
+
+        Ok(this)
+    }
+    async fn init_logger(
+        collector_endpoint: &Url,
+        resource: &Resource,
+    ) -> Result<SdkLoggerProvider, Box<dyn std::error::Error>> {
+        let log_endpoint = collector_endpoint.join("/v1/logs")?;
+
+        let exporter = LogExporter::builder()
+            .with_http()
+            .with_protocol(Protocol::HttpJson)
+            .with_endpoint(log_endpoint)
+            .build()?;
+
+        let processor = BatchLogProcessor::builder(exporter)
+            .with_batch_config(BatchConfig::default())
+            .build();
+
+        let logger_provider = SdkLoggerProvider::builder()
+            .with_log_processor(processor)
+            .with_resource(resource.clone())
+            .build();
+
+        Ok(logger_provider)
+    }
+    async fn init_meter(
+        collector_endpoint: &Url,
+        resource: &Resource,
+    ) -> Result<SdkMeterProvider, Box<dyn std::error::Error>> {
+        let metrics_endpoint = collector_endpoint.join("/v1/metrics")?;
+        let exporter = MetricExporter::builder()
+            .with_http()
+            .with_protocol(Protocol::HttpJson)
+            .with_endpoint(metrics_endpoint)
+            .build()?;
+
+        let reader = PeriodicReader::builder(exporter).build();
+
+        let provider = SdkMeterProvider::builder()
+            .with_resource(resource.clone())
+            .with_reader(reader)
+            .build();
+
+        Ok(provider)
+    }
+    fn shutdown(&self) {
+        let mut errors = vec![];
+
+        if let Some(ref provider) = self.meter_provider {
+            if let Err(err) = provider.shutdown() {
+                errors.push(format!("metrics: {err}"));
+            }
+        }
+
+        if let Some(ref provider) = self.logs_provider {
+            if let Err(err) = provider.shutdown() {
+                errors.push(format!("logs: {err}"));
+            }
+        }
+
+        if !errors.is_empty() {
+            eprintln!("Failed to shutdown providers: {}", errors.join("\n"))
+        }
+    }
 }
 
 #[rocket::get("/")]
@@ -106,61 +214,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .merge(Toml::file(config_path).nested())
         .extract()?;
 
+    let otel = Otel::from_config(&config).await?;
+
     // Logging
-    let log_file_path = config.log_directory.join("all.log");
+    if let Some(ref logs_provider) = otel.logs_provider {
+        tracing_subscriber::registry()
+            .with(OtelBridge::new(logs_provider).with_filter(EnvFilter::new("warn")));
+    } else {
+        let log_file_path = config.log_directory.join("all.log");
 
-    let log_file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(&log_file_path)
-        .map_err(|e| format!("{e}: {:?}", log_file_path))?;
+        let log_file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(&log_file_path)
+            .map_err(|e| format!("{e}: {:?}", log_file_path))?;
 
-    let log_endpoint = config.otlp.collector_endpoint.join("/v1/logs")?;
+        let fmt_layer = tracing_subscriber::fmt::layer()
+            .with_writer(log_file)
+            .compact()
+            .with_filter(EnvFilter::new("warn"));
 
-    let exporter = LogExporter::builder()
-        .with_http()
-        .with_protocol(Protocol::HttpJson)
-        .with_endpoint(log_endpoint)
-        .build()?;
+        tracing_subscriber::registry().with(fmt_layer);
 
-    let processor = BatchLogProcessor::builder(exporter)
-        .with_batch_config(BatchConfig::default())
-        .build();
-
-    let logger_provider = SdkLoggerProvider::builder()
-        .with_log_processor(processor)
-        .with_resource(get_resource(&config))
-        .build();
-
-    let otel_layer =
-        OpenTelemetryTracingBridge::new(&logger_provider).with_filter(EnvFilter::new("warn"));
-
-    let fmt_layer = tracing_subscriber::fmt::layer()
-        .with_writer(log_file)
-        .compact()
-        .with_filter(EnvFilter::new("warn"));
-
-    tracing_subscriber::registry()
-        .with(fmt_layer)
-        .with(otel_layer)
-        .init();
+        println!("Logging all outputs to {:?}", log_file_path);
+    }
 
     // Metrics
-    let metrics_endpoint = config.otlp.collector_endpoint.join("/v1/metrics")?;
-    let exporter = MetricExporter::builder()
-        .with_http()
-        .with_protocol(Protocol::HttpJson)
-        .with_endpoint(metrics_endpoint)
-        .build()?;
-
-    let reader = PeriodicReader::builder(exporter).build();
-
-    let provider = SdkMeterProvider::builder()
-        .with_resource(get_resource(&config))
-        .with_reader(reader)
-        .build();
-
-    global::set_meter_provider(provider.clone());
+    if let Some(meter_provider) = otel.meter_provider.clone() {
+        global::set_meter_provider(meter_provider)
+    }
 
     // Image Cache
     let image_cache = MmapImageCache::from_path(config.image_cache_directory)?;
@@ -188,24 +270,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         _ = tokio::signal::ctrl_c() => {
             println!("Received SIGINT. Requesting shutdown.");
 
-            let mut errors = vec![];
-
-            if let Err(err) = provider.shutdown() {
-                errors.push(format!("metrics: {err}"));
-            }
-
-            if let Err(err) = logger_provider.shutdown() {
-                errors.push(format!("logs: {err}"));
-            }
-
-            if !errors.is_empty() {
-                eprintln!(
-                    "Failed to shutdown providers: {}",
-                    errors.join("\n")
-                )
-            } else {
-                println!("Shutdown successful.")
-            }
+            otel.shutdown();
         }
     }
 
