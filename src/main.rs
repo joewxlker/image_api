df231bd437f1735d4a8b92bca36feca00583f1f5
joewxlker <joewxlk3r@gmail.com>
2026-04-21 18:53:33 +0400

    move cors fairing to middleware directory

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use tokio::select;
 use tracing::instrument::WithSubscriber;
 
 use crate::config::Config;
+use crate::middleware::cors::CORS;
 use crate::middleware::metrics::RequestMetricsFairing;
 use crate::routes::images;
 use crate::services::image_service::cache::MmapImageCache;
@@ -18,32 +19,6 @@ mod config;
 mod middleware;
 mod routes;
 mod services;
-
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::{Request, Response};
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, GET, PATCH, OPTIONS",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
 
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
