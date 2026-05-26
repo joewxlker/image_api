820b629336dbe26e98266bb2178e0a9b458c685d
joewxlker <joewxlk3r@gmail.com>
2026-05-26 12:22:32 +0400

    formatting

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -1,15 +1,16 @@
 use rocket::{Route, routes, serde::json::Json};
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ReadyState {
-    pub status: String
+    pub status: String,
 }
 
 #[rocket::get("/ready")]
 pub async fn get_ready_state() -> Json<ReadyState> {
-    Json(ReadyState { status: String::from("ready") })
+    Json(ReadyState {
+        status: String::from("ready"),
+    })
 }
 
 pub fn health_routes() -> Vec<Route> {
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,2 +1,2 @@
-pub mod images;
 pub mod health;
+pub mod images;
